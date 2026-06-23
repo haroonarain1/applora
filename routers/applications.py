b4dc8de8611c323fbd0c12c02c51eb2126225130
haroonarain1 <haroonarain06@gmail.com>
2026-06-22 20:14:49 -0500

    add GET and POST routes for applications

--- routers/applications.py
+++ routers/applications.py
@@ -17,3 +17,24 @@ def create_application(application: ApplicationCreate, db: Session = Depends(get
     db.refresh(new_application)
 
     return new_application
+
+@router.get("/applications", response_model=list[ApplicationResponse])
+def get_applications(db: Session = Depends(get_db)):
+    receive_app = db.query(Applications).all()
+    return receive_app
+
+@router.put("/applications/{id}", response_model=ApplicationResponse)
+def update_applications(id: int, application: ApplicationCreate, db: Session = Depends(get_db)):
+    put_app = db.query(Applications).filter(Applications.id == id).first()
+    put_app.company = application.company
+    put_app.role = application.role
+    put_app.salary = application.salary
+    put_app.date_applied = application.date_applied
+    put_app.status = application.status
+    put_app.notes = application.notes
+    user_id = 1
+
+    db.commit()
+    db.refresh(put_app)
+    return put_app
+
