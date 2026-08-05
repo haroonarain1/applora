2c38fe2f324a86884d6604e96443e01297d2da91
haroonarain1 <haroonarain06@gmail.com>
2026-08-04 22:24:34 -0500

    add dashboard with app fetching

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,37 @@
+import { useState, useEffect } from 'react'
+
+type Application = {
+    id: number
+    company: string
+    role: string
+    status: string
+    salary: number
+    date_applied: string
+    notes: string
+    user_id: number
+}
+
 export default function Dashboard(){
-    return(
-        <div>Dashboard</div>
+    
+    const [application, setApplication]  = useState<Application[]>([])
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        
+        fetch("http://localhost:8000/applications", {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => setApplication(data))
+    }, [])
+    return (
+        <div>
+            {application.map((app) => (
+                <div key={app.id}>
+                    {app.company}
+                </div>
+            ))}
+        </div>
     )
 }
