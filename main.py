760ecb6e1a998fec332d5cc6fce7d38ae2cf5fba
haroonarain1 <haroonarain06@gmail.com>
2026-08-06 21:25:55 -0500

    add Vercel URL to CORS allowed origins

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://applora-puce.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
